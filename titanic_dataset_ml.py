593bdb9376d7b171c82e8134339ab053936ae8aa
Utkarsh Srivastav <utkarshsrivastav233@gmail.com>
2021-03-23 12:56:15 +0530

    function created

--- titanic_dataset_ml.py
+++ titanic_dataset_ml.py
@@ -1,25 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-# pip install --user numpy
-# pip install --user pandas
-# pip install --user seaborn
-# pip install --user matplotliob
-# pip install --user sklearn
-
-
-# In[2]:
-
-
 import numpy as np
 import pandas as pd
-import seaborn as sns
-from matplotlib import pyplot as plt
-from matplotlib import style
-
 from sklearn import linear_model
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
@@ -31,257 +11,191 @@ from sklearn.svm import SVC
 from sklearn.naive_bayes import GaussianNB
 
 
-# In[3]:
+
+def load_data(path):
+    import pandas as pd
+
+    # path = "data/"
+
+    test_df = pd.read_csv(path + "test.csv")
+    train_df = pd.read_csv(path + "train.csv")
+    data = [train_df, test_df]
+    return data
 
 
 
-path = "data/"
-
-PREDICTION_LABEL = 'Survived'
-
-test_df = pd.read_csv(path + "test.csv")
-train_df = pd.read_csv(path + "train.csv")
-
-
-# In[4]:
-
-
-total = train_df.isnull().sum().sort_values(ascending=False)
-percent_1 = train_df.isnull().sum()/train_df.isnull().count()*100
-percent_2 = (round(percent_1, 1)).sort_values(ascending=False)
-missing_data = pd.concat([total, percent_2], axis=1, keys=['Total', '%'])
-missing_data.head(5)
+def data_processing(data):
+    for dataset in data:
+        dataset['relatives'] = dataset['SibSp'] + dataset['Parch']
+        dataset.loc[dataset['relatives'] > 0, 'not_alone'] = 0
+        dataset.loc[dataset['relatives'] == 0, 'not_alone'] = 1
+        dataset['not_alone'] = dataset['not_alone'].astype(int)
+    # train_df['not_alone'].value_counts()
+    data[0] = data[0].drop(['PassengerId'], axis=1)
+    return data
 
 
-# In[5]:
+
+def data_cleaning(data):
+    import re
+    deck = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "U": 8}
+
+    for dataset in data:
+        dataset['Cabin'] = dataset['Cabin'].fillna("U0")
+        dataset['Deck'] = dataset['Cabin'].map(lambda x: re.compile("([a-zA-Z]+)").search(x).group())
+        dataset['Deck'] = dataset['Deck'].map(deck)
+        dataset['Deck'] = dataset['Deck'].fillna(0)
+        dataset['Deck'] = dataset['Deck'].astype(int)
+        
+    data[0] = data[0].drop(['Cabin'], axis=1)
+    data[1] = data[1].drop(['Cabin'], axis=1)
+    return data
 
 
-data = [train_df, test_df]
-for dataset in data:
-    dataset['relatives'] = dataset['SibSp'] + dataset['Parch']
-    dataset.loc[dataset['relatives'] > 0, 'not_alone'] = 0
-    dataset.loc[dataset['relatives'] == 0, 'not_alone'] = 1
-    dataset['not_alone'] = dataset['not_alone'].astype(int)
-train_df['not_alone'].value_counts()
+
+def data_cleaning2(data):
+    import numpy as np
+
+    for dataset in data:
+        mean = data[0]["Age"].mean()
+        std = data[1]["Age"].std()
+        is_null = dataset["Age"].isnull().sum()
+        
+        rand_age = np.random.randint(mean - std, mean + std, size = is_null)
+        
+        age_slice = dataset["Age"].copy()
+        age_slice[np.isnan(age_slice)] = rand_age
+        dataset["Age"] = age_slice
+        dataset["Age"] = data[0]["Age"].astype(int)
+
+    common_value = 'S'
+    for dataset in data:
+        dataset['Embarked'] = dataset['Embarked'].fillna(common_value)
+
+    for dataset in data:
+        dataset['Fare'] = dataset['Fare'].fillna(0)
+        dataset['Fare'] = dataset['Fare'].astype(int)
+
+    titles = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
+    for dataset in data:
+        # extract titles
+        dataset['Title'] = dataset.Name.str.extract(' ([A-Za-z]+)\.', expand=False)
+        # replace titles with a more common title or as Rare
+        dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess','Capt', 'Col','Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+        dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
+        dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+        dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
+        # convert titles into numbers
+        dataset['Title'] = dataset['Title'].map(titles)
+        # filling NaN with 0, to get safe
+        dataset['Title'] = dataset['Title'].fillna(0)
+    data[0] = data[0].drop(['Name'], axis=1)
+    data[1] = data[1].drop(['Name'], axis=1)
+
+    genders = {"male": 0, "female": 1}
+    for dataset in data:
+        dataset['Sex'] = dataset['Sex'].map(genders)
+
+    data[0] = data[0].drop(['Ticket'], axis=1)
+    data[1] = data[1].drop(['Ticket'], axis=1)
+
+    ports = {"S": 0, "C": 1, "Q": 2}
+    for dataset in data:
+        dataset['Embarked'] = dataset['Embarked'].map(ports)
 
 
-# In[6]:
+    for dataset in data:
+        dataset['Age'] = dataset['Age'].astype(int)
+        dataset.loc[ dataset['Age'] <= 11, 'Age'] = 0
+        dataset.loc[(dataset['Age'] > 11) & (dataset['Age'] <= 18), 'Age'] = 1
+        dataset.loc[(dataset['Age'] > 18) & (dataset['Age'] <= 22), 'Age'] = 2
+        dataset.loc[(dataset['Age'] > 22) & (dataset['Age'] <= 27), 'Age'] = 3
+        dataset.loc[(dataset['Age'] > 27) & (dataset['Age'] <= 33), 'Age'] = 4
+        dataset.loc[(dataset['Age'] > 33) & (dataset['Age'] <= 40), 'Age'] = 5
+        dataset.loc[(dataset['Age'] > 40) & (dataset['Age'] <= 66), 'Age'] = 6
+        dataset.loc[ dataset['Age'] > 66, 'Age'] = 6
 
 
-train_df = train_df.drop(['PassengerId'], axis=1)
+    for dataset in data:
+        dataset.loc[ dataset['Fare'] <= 7.91, 'Fare'] = 0
+        dataset.loc[(dataset['Fare'] > 7.91) & (dataset['Fare'] <= 14.454), 'Fare'] = 1
+        dataset.loc[(dataset['Fare'] > 14.454) & (dataset['Fare'] <= 31), 'Fare']   = 2
+        dataset.loc[(dataset['Fare'] > 31) & (dataset['Fare'] <= 99), 'Fare']   = 3
+        dataset.loc[(dataset['Fare'] > 99) & (dataset['Fare'] <= 250), 'Fare']   = 4
+        dataset.loc[ dataset['Fare'] > 250, 'Fare'] = 5
+        dataset['Fare'] = dataset['Fare'].astype(int)
 
 
-# In[7]:
+    for dataset in data:
+        dataset['Age_Class']= dataset['Age']* dataset['Pclass']
 
 
-import re
-deck = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "U": 8}
-data = [train_df, test_df]
+    for dataset in data:
+        dataset['Fare_Per_Person'] = dataset['Fare']/(dataset['relatives']+1)
+        dataset['Fare_Per_Person'] = dataset['Fare_Per_Person'].astype(int)
 
-for dataset in data:
-    dataset['Cabin'] = dataset['Cabin'].fillna("U0")
-    dataset['Deck'] = dataset['Cabin'].map(lambda x: re.compile("([a-zA-Z]+)").search(x).group())
-    dataset['Deck'] = dataset['Deck'].map(deck)
-    dataset['Deck'] = dataset['Deck'].fillna(0)
-    dataset['Deck'] = dataset['Deck'].astype(int)
+    train_df = data[1]
+    return train_df
+
+
+
+def clean_data(train_df):
+    train_labels = train_df['Survived']
+    train_df = train_df.drop('Survived', axis=1)
+
+    train_data = [train_df, train_labels]
+    return train_data
+
+
+
+def random_forest1(train_data):
     
-train_df = train_df.drop(['Cabin'], axis=1)
-test_df = test_df.drop(['Cabin'], axis=1)
+    random_forest = RandomForestClassifier(n_estimators=100)
+    random_forest.fit(train_data[0], train_data[1])
+    acc_random_forest = round(random_forest.score(train_data[0], train_data[1]) * 100, 2)
+    return acc_random_forest
 
 
-# In[8]:
 
+def logreg1(train_data):
+    logreg = LogisticRegression(solver='lbfgs', max_iter=110)
+    logreg.fit(train_data[0], train_data[1])
+    acc_log = round(logreg.score(train_data[0], train_data[1]) * 100, 2)
+    return acc_log
 
-data = [train_df, test_df]
 
-for dataset in data:
-    mean = train_df["Age"].mean()
-    std = test_df["Age"].std()
-    is_null = dataset["Age"].isnull().sum()
-    
-    rand_age = np.random.randint(mean - std, mean + std, size = is_null)
-    
-    age_slice = dataset["Age"].copy()
-    age_slice[np.isnan(age_slice)] = rand_age
-    dataset["Age"] = age_slice
-    dataset["Age"] = train_df["Age"].astype(int)
-train_df["Age"].isnull().sum()
 
+def gaussian1(train_data):
+    gaussian = GaussianNB()
+    gaussian.fit(train_data[0], train_data[1])
+    acc_gaussian = round(gaussian.score(train_data[0], train_data[1]) * 100, 2)
+    return acc_gaussian
 
-# In[9]:
 
 
-common_value = 'S'
-data = [train_df, test_df]
+def linear_svc1(train_data):
+    linear_svc = SVC(gamma='auto')
+    linear_svc.fit(train_data[0], train_data[1])
+    acc_linear_svc = round(linear_svc.score(train_data[0], train_data[1]) * 100, 2)
+    return acc_linear_svc
 
-for dataset in data:
-    dataset['Embarked'] = dataset['Embarked'].fillna(common_value)
 
 
-# In[10]:
+def decision_tree1(train_data):
+    decision_tree = DecisionTreeClassifier()
+    decision_tree.fit(train_data[0], train_data[1])
+    acc_decision_tree = round(decision_tree.score(train_data[0], train_data[1]) * 100, 2)
+    return acc_decision_tree
 
 
-data = [train_df, test_df]
 
-for dataset in data:
-    dataset['Fare'] = dataset['Fare'].fillna(0)
-    dataset['Fare'] = dataset['Fare'].astype(int)
-
-
-# In[11]:
-
-
-data = [train_df, test_df]
-titles = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
-
-for dataset in data:
-    # extract titles
-    dataset['Title'] = dataset.Name.str.extract(' ([A-Za-z]+)\.', expand=False)
-    # replace titles with a more common title or as Rare
-    dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess','Capt', 'Col','Don', 'Dr',                                            'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
-    dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
-    dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
-    dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
-    # convert titles into numbers
-    dataset['Title'] = dataset['Title'].map(titles)
-    # filling NaN with 0, to get safe
-    dataset['Title'] = dataset['Title'].fillna(0)
-train_df = train_df.drop(['Name'], axis=1)
-test_df = test_df.drop(['Name'], axis=1)
-
-
-# In[12]:
-
-
-genders = {"male": 0, "female": 1}
-data = [train_df, test_df]
-
-for dataset in data:
-    dataset['Sex'] = dataset['Sex'].map(genders)
-
-
-# In[13]:
-
-
-train_df = train_df.drop(['Ticket'], axis=1)
-test_df = test_df.drop(['Ticket'], axis=1)
-
-
-# In[14]:
-
-
-ports = {"S": 0, "C": 1, "Q": 2}
-data = [train_df, test_df]
-
-for dataset in data:
-    dataset['Embarked'] = dataset['Embarked'].map(ports)
-
-
-# In[15]:
-
-
-data = [train_df, test_df]
-for dataset in data:
-    dataset['Age'] = dataset['Age'].astype(int)
-    dataset.loc[ dataset['Age'] <= 11, 'Age'] = 0
-    dataset.loc[(dataset['Age'] > 11) & (dataset['Age'] <= 18), 'Age'] = 1
-    dataset.loc[(dataset['Age'] > 18) & (dataset['Age'] <= 22), 'Age'] = 2
-    dataset.loc[(dataset['Age'] > 22) & (dataset['Age'] <= 27), 'Age'] = 3
-    dataset.loc[(dataset['Age'] > 27) & (dataset['Age'] <= 33), 'Age'] = 4
-    dataset.loc[(dataset['Age'] > 33) & (dataset['Age'] <= 40), 'Age'] = 5
-    dataset.loc[(dataset['Age'] > 40) & (dataset['Age'] <= 66), 'Age'] = 6
-    dataset.loc[ dataset['Age'] > 66, 'Age'] = 6
-
-
-# In[16]:
-
-
-data = [train_df, test_df]
-
-for dataset in data:
-    dataset.loc[ dataset['Fare'] <= 7.91, 'Fare'] = 0
-    dataset.loc[(dataset['Fare'] > 7.91) & (dataset['Fare'] <= 14.454), 'Fare'] = 1
-    dataset.loc[(dataset['Fare'] > 14.454) & (dataset['Fare'] <= 31), 'Fare']   = 2
-    dataset.loc[(dataset['Fare'] > 31) & (dataset['Fare'] <= 99), 'Fare']   = 3
-    dataset.loc[(dataset['Fare'] > 99) & (dataset['Fare'] <= 250), 'Fare']   = 4
-    dataset.loc[ dataset['Fare'] > 250, 'Fare'] = 5
-    dataset['Fare'] = dataset['Fare'].astype(int)
-
-
-# In[17]:
-
-
-data = [train_df, test_df]
-for dataset in data:
-    dataset['Age_Class']= dataset['Age']* dataset['Pclass']
-
-
-# In[18]:
-
-
-for dataset in data:
-    dataset['Fare_Per_Person'] = dataset['Fare']/(dataset['relatives']+1)
-    dataset['Fare_Per_Person'] = dataset['Fare_Per_Person'].astype(int)
-# Let's take a last look at the training set, before we start training the models.
-# train_df.head(10)
-
-
-# In[19]:
-
-
-train_labels = train_df[PREDICTION_LABEL]
-train_df = train_df.drop(PREDICTION_LABEL, axis=1)
-
-
-# In[20]:
-
-
-random_forest = RandomForestClassifier(n_estimators=100)
-random_forest.fit(train_df, train_labels)
-acc_random_forest = round(random_forest.score(train_df, train_labels) * 100, 2)
-
-
-# In[21]:
-
-
-logreg = LogisticRegression(solver='lbfgs', max_iter=110)
-logreg.fit(train_df, train_labels)
-acc_log = round(logreg.score(train_df, train_labels) * 100, 2)
-
-
-# In[22]:
-
-
-gaussian = GaussianNB()
-gaussian.fit(train_df, train_labels)
-acc_gaussian = round(gaussian.score(train_df, train_labels) * 100, 2)
-
-
-# In[23]:
-
-
-linear_svc = SVC(gamma='auto')
-linear_svc.fit(train_df, train_labels)
-acc_linear_svc = round(linear_svc.score(train_df, train_labels) * 100, 2)
-
-
-# In[24]:
-
-
-decision_tree = DecisionTreeClassifier()
-decision_tree.fit(train_df, train_labels)
-acc_decision_tree = round(decision_tree.score(train_df, train_labels) * 100, 2)
-
-
-# In[25]:
-
-
-results = pd.DataFrame({
-    'Model': ['Support Vector Machines', 'logistic Regression',
-              'Random Forest', 'Naive Bayes', 'Decision Tree'],
-    'Score': [acc_linear_svc, acc_log,
-              acc_random_forest, acc_gaussian, acc_decision_tree]})
-result_df = results.sort_values(by='Score', ascending=False)
-result_df = result_df.set_index('Score')
-print(result_df)
+def results1(acc_linear_svc, acc_log, acc_random_forest, acc_gaussian, acc_decision_tree):
+    results = pd.DataFrame({
+        'Model': ['Support Vector Machines', 'logistic Regression',
+                'Random Forest', 'Naive Bayes', 'Decision Tree'],
+        'Score': [acc_linear_svc, acc_log,
+                acc_random_forest, acc_gaussian, acc_decision_tree]})
+    result_df = results.sort_values(by='Score', ascending=False)
+    result_df = result_df.set_index('Score')
+    print(result_df)
 
